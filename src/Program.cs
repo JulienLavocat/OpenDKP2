8865c55bcc6f7d1bafc083b6cb71b94fad55ee82
Julien Lavocat <julien.lavocat@gmail.com>
2025-03-14 18:06:58 +0100

    feat: I missed my swagger <3

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,17 @@
 using OpenDKPShared.DBModels;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<OpenDkpContext>();
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
+
+app.MapOpenApi();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+});
 
 app.MapGet(
     "/",
